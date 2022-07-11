48a11e1856bd92dd9223aeaa60df33c780e05b9d
Ramin <sahelram@gmail.com>
2022-07-11 20:17:37 +0430

    add content(dashboard-more-sales-visitors)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
-import { useState } from 'react';
-import logo from './logo.svg'
-import './App.css'
-
+import Content_dashboard from './Content/Content-Dashboard'
+import Content_more from './Content/Content-more'
+import Content_sales from './Content/Content-sales'
+import Content_visitors from './Content/Content-visitors'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <main className="flex" >
+      <aside className="w-1/6 bg-slate-500 h-screen">
+      </aside>
+      <section className="w-5/6 bg-orange-300 h-screen">
+      
+      <section className="bg-slate-100 h-screen w-full ">
+          <Content_dashboard/>
+          <Content_more/>
+          <Content_sales/>
+          <Content_visitors/>
+           </section>
+          
+      </section>
+        </main>
+    )
 }
 
 export default App
